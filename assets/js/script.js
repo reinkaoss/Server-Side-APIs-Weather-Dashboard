3e2f8d21627d845d738a87f960839a79cf740b4d
victorbiscio <victorbiscio1@hotmail.com>
2022-12-22 21:47:51 +0000

    Pushing changes to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,14 @@
+var input = $('#search-input').val();
 var API_KEY = "a09dbdd9e81585fea0ea468d539562cf";
-var queryURL = "http://api.openweathermap.org/geo/1.0/direct?appid=" + API_KEY + "&q="
-
-
-
-
-// 
-// response.city.name
-// response.
-// response.dt -> Date needs to be converted
-// response.main.temp -> Convert to Celsius -> To convert from Kelvin to Celsius: C = K - 273.15
-// response.main.humidity
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + input + '&APPID=a09dbdd9e81585fea0ea468d539562cf'
 
 // Add event listener to the search button 
 $("#mainSearch").on("click", function(event) {
     event.preventDefault();
-    var input = $('#search-input');
-    console.log(input.val())
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?&appid=" + API_KEY + input;
+    var input = $('#search-input').val();
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + input + '&APPID=a09dbdd9e81585fea0ea468d539562cf'
 
+    
 // Display input on textarea containing all the variables 
 // Loop through the past 6 inputs and add buttons for each one 
 
@@ -28,13 +19,31 @@ $.ajax({
 }).then(function(response) {
 // Create variables for every element i.e temp, date, city, wind, humidity
 // Use localstorage to save user's data 
+
+
+var weather = response.main.temp
+weather = weather - 273.15;
+weatherNew = weather.toFixed(0) + "°C";
+var wind = response.wind.speed + "KPH"
+var humidity = response.main.humidity + "%"
+
 var input = $('#search-input').val();
-var city = response.city;
-$('#output').text(city);
-console.log(response)
+var city = response.name;
+$('#output').text(city + " " + moment().format("dddd, MMMM Do YYYY"));
+$('article').append(city)
+$('.temp').text(weatherNew)
+$('.wind').text(wind)
+$('.humidity').text(humidity)
+// $('.temp').append(city)
+    
+
+
+});
+// console.log(response.city)
+
     
 });
-});
+
 
 function renderButtons() {
     $('#search-input').empty();
@@ -43,4 +52,37 @@ function renderButtons() {
 
 renderButtons();
 
+// var city = $('#city').val();
+// API_KEY = 'a09dbdd9e81585fea0ea468d539562cf';
+// var queryURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=fa09dbdd9e81585fea0ea468d539562cf";
 
+// $(document).ready(function() {
+//     $('#weather-form').submit(function(event) {
+//       event.preventDefault();
+  
+//       var city = $('#city').val();
+//       var date = moment();
+//       $.ajax({
+//         url: queryURL,
+//         method: "GET",
+//         },
+//         console.log(response)
+//         function(response) {
+//           // Update the current weather section
+//           $('#city-name').text(response.name);
+//           $('#date').text(date);
+//           $('#weather-icon').attr('src', 'https://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
+//           $('#temperature').text(response.main.temp + '°F');
+//           $('#humidity').text(response.main.humidity + '%');
+//           $('#wind-speed').text(response.wind.speed + ' mph');
+//                     // Update the 5-Day Forecast
+//                     $('.date').text(date);
+//                     $('.temperature').text(response.main.temp + '°F');
+//           $('.humidity').text(response.main.humidity + '%');
+//           $('.wind-speed').text(response.wind.speed + ' mph');
+//           $('.weather-icon').attr('src', 'https://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
+//         });
+//       });
+//     });
+
+          
