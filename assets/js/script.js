7b5b4e7fc3dd48e6dd62ed1b0cddd608d464aa74
victorbiscio <victorbiscio1@hotmail.com>
2023-01-02 15:10:56 +0000

    Trying to fix localstorage and buttons glitch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
+var input = $("#search-input").val();
+var queryURL =
+"https://api.openweathermap.org/data/2.5/forecast?q=";
+var API_KEY = '&APPID=a09dbdd9e81585fea0ea468d539562cf';
+
 // Add event listener to the search button
 $("#mainSearch").on("click", function (event) {
   event.preventDefault();
   var input = $("#search-input").val();
+  weatherAPIFunction(input);
+});
 
-  var queryURL =
-    "https://api.openweathermap.org/data/2.5/forecast?q=" +
-    input +
-    "&APPID=a09dbdd9e81585fea0ea468d539562cf";
-
+function weatherAPIFunction(input) {
   $.ajax({
-    url: queryURL,
+    url: queryURL + input + API_KEY,
     method: "GET",
   }).then(function (response) {
+
     // Setting variables for all data
     var weather = response.list[0].main.temp;
     weather = weather - 273.15;
@@ -19,7 +23,6 @@ $("#mainSearch").on("click", function (event) {
     var wind = response.list[0].wind.speed + "KPH";
     var humidity = response.list[0].main.humidity + "%";
 
-    console.log(response)
     // Getting weather icon
     var iconCode = response.list[0].weather[0].icon;
 
@@ -30,7 +33,7 @@ $("#mainSearch").on("click", function (event) {
     $("#weather-icon").append(iconImg);
 
     // Generating the main view
-    var input = $("#search-input").val();
+    // var input = $("#search-input").val();
     var city = response.city.name;
     $("#output").text(city);
     $("#date").text(moment().format("dddd, MMMM Do YYYY"));
@@ -38,14 +41,19 @@ $("#mainSearch").on("click", function (event) {
     $(".wind").text("Wind: " + wind);
     $(".humidity").text("humidity: " + humidity);
 
-    var input = $("#search-input").val();
-    for (var i = 0; i < 6; i++)
-      if (i < 6) var histButton = $("<button>").text(input);
-    $(histButton).attr("id", "histSearch");
-    $(histButton).attr("data", input);
-    $(histButton).addClass("btn btn-primary btn-sm");
-    $("#history").append(histButton);
-
+    // var input = $("#search-input").val();
+    var histButtonList = [];
+    for (var i = 0; i < 1; i++) {
+      var histButton = $("<button>").text(input); 
+      if(!(histButton in histButtonList)) {
+        histButtonList.push(histButton)
+        $("#history").append(histButton);
+      $(histButton).attr("id", "histSearch");
+      $(histButton).attr("data", input);
+      $(histButton).addClass("btn btn-primary btn-sm");
+      localStorage.setItem('userInput', JSON.stringify(histButton.text()))
+    }
+}
     // Get next 5 days with moment js
     var dates = [];
     for (var i = 1; i <= 6; i++) {
@@ -96,82 +104,46 @@ $("#mainSearch").on("click", function (event) {
       $(windForecastEl[index]).text("Wind: " + wind + "KPH")
     );
 
-    // Adding the same function to the history
-    $(histButton).on("click", function () {
-      var histButtonValue = histButton.attr("data");
-      input = histButtonValue;
+// 5 days forecast icons
+// var iconUrl = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
 
-      $("#output").text(city);
-      $("#date").text(moment().format("dddd, MMMM Do YYYY"));
-      $(".temp").text("Temperature: " + weatherNew);
-      $(".wind").text("Wind: " + wind);
-      $(".humidity").text("humidity: " + humidity);
 
-      // Updating 5 days forecast from main input
-      $(".card-title").text(city);
+// Loop through 5 days icons
+var iconForecastEl = $("#weathericonForecast");
 
-      // Loop through 5 days temperatures
-      var forecastEl = [
-        response.list[3].main.temp,
-        response.list[11].main.temp,
-        response.list[19].main.temp,
-        response.list[27].main.temp,
-        response.list[35].main.temp,
-      ];
+var iconEl = [  response.list[3].weather[0].icon,
+  response.list[11].weather[0].icon,
+  response.list[19].weather[0].icon,
+  response.list[27].weather[0].icon,
+  response.list[35].weather[0].icon,
+];
 
-      // get the temperature elements
-      var tempElements = $(".tempForecast");
-
-      // loop through the forecastEl array
-      forecastEl.forEach((temp, index) => {
-        // convert the temperature from kelvins to degrees Celsius
-        temp = temp - 273.15;
-        // round the temperature to the nearest integer
-        temp = temp.toFixed(0);
-        // format the temperature as a string
-        var tempString = temp + "°C";
-        // update the text of the element with the class "tempForecast"
-        $(tempElements[index]).text(tempString);
-      });
-
-      // Wind 5 days forecast
-      var windForecastEl = $(".windForecast");
-
-      var windEl = [
-        response.list[3].wind.speed,
-        response.list[11].wind.speed,
-        response.list[19].wind.speed,
-        response.list[27].wind.speed,
-        response.list[35].wind.speed,
-      ];
-
-      windEl.forEach((wind, index) =>
-        $(windForecastEl[index]).text("Wind: " + wind + " KPH")
-      );
-    });
-
-    // Humidity 5 days forecast
-    var humidityForecastEl = $(".humidityForecast");
-    var humidityEl = [
-      response.list[3].main.humidity,
-      response.list[11].main.humidity,
-      response.list[19].main.humidity,
-      response.list[27].main.humidity,
-      response.list[35].main.humidity,
-    ];
-
-    humidityEl.forEach((humidity, index) =>
-      $(humidityForecastEl[index]).text("Humidity: " + humidity + " %")
-    );
-  });
+// loop through the iconEl array
+iconEl.forEach((icon, index) => {
+  // Build the icon URL using the icon code
+  var iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+  // Set the src attribute of the image element
+  $(iconForecastEl[index]).attr("src", iconUrl);
 });
+
+$(histButton).on("click", function (event) {
+    event.preventDefault();
+    histButton.empty()
+  weatherAPIFunction(input);
+      });    
+    });
+}
 
 $(document).ready(function () {
 
-  var input = localStorage.getItem("userInput");
+  var input = (JSON.parse(localStorage.getItem("userInput")));
 
   if (input) {
-    $("#search-input").val(input);
+    histButton = $("<button>").text(input);
+    $("#history").append(histButton);
+    $(histButton).attr("id", "histSearch");
+    $(histButton).addClass("btn btn-primary btn-sm");
+    weatherAPIFunction(input);
   }
 });
 
